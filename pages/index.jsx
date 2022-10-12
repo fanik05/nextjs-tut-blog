a79502e3cd98fbe682a91820c596c32e966d146c
fanik05 <fatinanik129@gmail.com>
2022-10-12 17:01:28 +0600

    dynamic routes work initialized

---
+++ pages/index.jsx
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hey, I&apos;m <strong>Fatin</strong>. I am a{' '}
+          <em>software sngineer</em> who specializes in <em>JavaScript</em>. I
+          am also an avid gamer 🎮. I like playing <em>DOTA 2</em> the most.
+          Stay tuned 🪶🪶.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
